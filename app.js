637d572630cb8212854d8a1108ab78b6a89107a0
cumar3023 <cumarmaxamed3023@gmail.com>
2023-07-28 08:37:15 -0700

    api resloved

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors=require('cors')
 require('dotenv').config()
 const app=express()
 app.use(express.json());
-app.use(cors({origin:['http://localhost:5173','https://beta-house-fawn.vercel.app/']}))
+app.use(cors({origin:['http://localhost:5173','https://beta-house-fawn.vercel.app']}))
 // connecting to mongodb server
 mongoose.set('strictQuery', false);
 
