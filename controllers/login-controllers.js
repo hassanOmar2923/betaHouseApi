799ce4ee7ce36e7c96e5f6c120e4fc4003265169
cumar3023 <cumarmaxamed3023@gmail.com>
2023-08-14 17:39:17 +0300

    removed Login

--- controllers/login-controllers.js
+++ controllers/login-controllers.js
@@ -38,7 +38,7 @@ const login = async (req, res) => {
             id: usergetdata._id,
             name: usergetdata.name,
             email: usergetdata.email,
-            role:usergetdata.role,
+           
         
           },
           process.env.token
